ba29f08ba726b4c053a176a10795bf0afc1be304
Isaiah2am <isaiahkoroma@hotmail.com>
2024-11-26 10:24:50 +0100

    commit

--- Functions/Functions/Program.cs
+++ Functions/Functions/Program.cs
@@ -4,10 +4,31 @@ namespace Functions
 {
     internal class Program
     {
+        string[] vragen = new string[]
+        {
+            "ben je l?",
+            "ben je w?"
+
+        };
+        internal string getvraag(int vraagindex)
+        {
+            return vragen[vraagindex];
+        }
+
+
+        internal string getrandomvraag()
+        {
+            //1 maak eem new random object
+            Random rand = new Random();
+            int random = rand.Next(vragen.Length);
+            return getvraag(random);
+        }
+
         static void Main(string[] args)
         {
-           Program program = new Program();
-           program.run();
+            Program program = new Program();
+            program.run();
+
             //class
         }
         internal void run()
@@ -18,6 +39,16 @@ namespace Functions
             vraag3();
             vraag4();
             vraag5();
+            vraag6();
+            string vraag0 = getvraag(0);
+            Console.WriteLine(vraag0);
+            string vraagr= getrandomvraag();
+
+            Console.WriteLine(vraagr);
+            
+
+
+
         }
         internal void vraag1()
         {
@@ -56,6 +87,21 @@ namespace Functions
             Console.WriteLine("What is the worst game you played but that you liked anyway?");
             string antwoord = Console.ReadLine();
 
+            Console.WriteLine(antwoord);
+
+        }
+        internal void vraag6()
+        {
+            Console.WriteLine("What would you call a male human?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+
+        }
+        internal void vraag7()
+        {
+            Console.WriteLine("How long do you think you can survive a zombie apocolypse?");
+            string antwoord = Console.ReadLine();
             Console.WriteLine(antwoord);
 
         }
