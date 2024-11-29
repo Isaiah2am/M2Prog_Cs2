54664cbcdbda35406fd7b1aef6922bd1cfb62636
Isaiah2am <isaiahkoroma@hotmail.com>
2024-11-29 10:07:23 +0100

    commit

--- RijSchool/ConsoleApp1/Program.cs
+++ RijSchool/ConsoleApp1/Program.cs
@@ -1,23 +1,64 @@
 ﻿namespace ConsoleApp1
 {
     internal class Program;
-    internal class auto;
-    internal class rijleraar;
-    internal class student;
-    internal class lespakket;
-    internal class rijtest;
-    internal class theorietest;
+    internal class auto
+    {
+        internal bool automaat;
+        internal string merk;
+        internal string kenteken;
+        internal int kilometerstand;
+    };
+    internal class rijleraar
+    {
+        internal string naam;
+        internal int leeftijd;
+        internal bool zzp;
+    }
+    internal class student
+    {
+        internal string naam;
+        internal int leeftijd;
+        internal lespakket lespakket;
+        internal rijleraar leeraar;
+        internal theorietest theorietest;
+        internal rijtest rijtest;
+    }
+    internal class lespakket
+    {
+        internal int urengekocht;
+        internal int urenverbruikt;
+        internal int examenpoging;
+        internal bool automaat;
+    }
+    internal class rijtest
+    {
+        internal bool datum;
+        internal lesuur[] lesuren;
+    }
+    internal class theorietest
+    {
+        internal int aantalfouten;
+        internal bool gehaald;
+        internal DateTime afnamedatum;
+    }
+    internal class lesuur 
+    {
+        internal int tijd;
+        internal student student;
+        internal rijleraar rijleraar;
+        internal auto auto;
+    }
+
+
     internal class Dag;
     internal class Myclass
-
-    {
-
-    }
-    LesUur
     {
         static void Main(string[] args)
         {
             
+
+
+
         }
     }
 }
