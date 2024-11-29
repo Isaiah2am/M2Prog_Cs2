85ff0a011479181fb34f9a13a0173cf72100fa5a
Isaiah2am <isaiahkoroma@hotmail.com>
2024-11-29 09:42:46 +0100

    class commit

---
+++ RijSchool/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program;
+    internal class auto;
+    internal class rijleraar;
+    internal class student;
+    internal class lespakket;
+    internal class rijtest;
+    internal class theorietest;
+    internal class Dag;
+    internal class Myclass
+
+    {
+
+    }
+    LesUur
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
