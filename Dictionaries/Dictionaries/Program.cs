845c9f38a8a3632982fefd573a73d6948b0e2713
Isaiah2am <isaiahkoroma@hotmail.com>
2025-01-17 09:46:09 +0100

    commit dictionary 5.1

---
+++ Dictionaries/Dictionaries/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Dictionaries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int,string> personeelnummer = new Dictionary<int,string>();
+
+            personeelnummer.Add(382942, "jim");
+            personeelnummer.Add(382941, "zim");
+         
+            
+            string jim = personeelnummer[382942];
+            string zim = personeelnummer[382941];
+
+            foreach (int key in personeelnummer.Keys)
+            {
+                Console.WriteLine(key);
+            }
+
+            foreach (string value in personeelnummer.Values)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
